994bae7e3d68be348229249177e3352ef60bb8e8
YanFelipy22 <yan-felipy@hotmail.com>
2025-05-14 00:14:42 -0300

    Integrando Firebase ao Projeto

---
+++ firebase/config.jsx
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBS7824nBYVdqIlOj7mL-4muwj66LmK3Qk",
+    authDomain: "myminiblog-1f727.firebaseapp.com",
+    projectId: "myminiblog-1f727",
+    storageBucket: "myminiblog-1f727.firebasestorage.app",
+    messagingSenderId: "502337539304",
+    appId: "1:502337539304:web:8088829572a8103eff8d5d"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db }
