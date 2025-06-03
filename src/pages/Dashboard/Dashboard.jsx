8d95502eadf8dfa1d27682a3e8274e47e4e81786
YanFelipy22 <yan-felipy@hotmail.com>
2025-06-03 01:51:24 -0300

    Erros de formulário e Posts de usuários no feed

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,46 +2,91 @@ import styles from './Dashboard.module.css'
 import avatar from '../../assets/avatar.png'
 import putimgs from '../../assets/put_image.svg'
 
-import { useState } from 'react'
+import { collection, onSnapshot } from 'firebase/firestore'
+import { db } from '../../../firebase/config'; 
+
+
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuthValue } from '../../../context/AuthContext'
-import {useAuthentication} from'../../hooks/useAuthentication'
+
 import { useInsertDocument } from '../../hooks/useInsertDocument'
 
 
 const Dashboard = () => {
+  
+  const navigate = useNavigate()
+    const { insertDocument, response } = useInsertDocument("posts")
 
-  const {insertDocument, response} = useInsertDocument("posts")
+
+    const { user } = useAuthValue()
+  
+    const [body, setBody] = useState("")
+    const [image, setImage] = useState("")
+    const [tags, setTags] = useState([])
+    const [posts, setPosts] = useState([])
+    
+    const [formError, setFormError] = useState("")
+
+    //GET POSTS
+  
+     useEffect(() => {
+       const fetchItems = async () => {
+         
+         try {
+            const unsubscribe = onSnapshot(collection(db, "posts"), (snapshot) => {
+         const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+         setPosts(data);
+         return () => unsubscribe();
+       });
+         } catch (error) {
+           console.error("Error fetching items:", error);
+         }
+       };
+       fetchItems();
+
+     
+       
+     }, []);
+
+  
+       
+    const handleSubmitPost = (e) => {
+      e.preventDefault()
+      setFormError("")
 
 
-  const {user} = useAuthValue()
 
-  const [body, setBody] = useState("")
-const [image, setImage] = useState("")
-const [tags, setTags] = useState([])
-const [formError, setFormError] = useState([])
+    //ARRAY OF TAGS
+    const tagsArray = tags.split(",").map((tag)=> tag.trim().toLowerCase())
+    console.log(tagsArray)
+    //CHECK VALUES 
+    if(body || tags == "") {
+      setFormError("* Por favor, preencha todos os campos")
+    }  
+    //VALIDATE IMAGE
 
-  const handleSubmitPost = (e) => {
-    e.preventDefault()
-setFormError("")
+    try {
+      new URL(image)
+    } catch (error) {
+      setFormError("* A imagem precisa ser uma URL ")
+    }
 
-insertDocument({
-  image,
-  body,
-  tags,
-  uid: user.uid,
-  createdBy: user.displayName
-})
-console.log(response)
-console.log(  
-  image,
-  body,
-  tags,
- user.uid,
-  user.displayName )
+    if (formError) return
 
+    insertDocument({
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName
+    })
+  
+
+     
+      navigate("/")
   }
-
+console.log(posts)
 
   return (
     <main>
@@ -60,44 +105,86 @@ console.log(
             <form onSubmit={handleSubmitPost} className={styles.post_form}>
 
               <label>
-                <textarea required value={body} onChange={(e)=> setBody(e.target.value)} name="post" maxLength='200' placeholder="Diga ao mundo como se sente hoje..." id="">
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)} name="post" maxLength='200' placeholder="Diga ao mundo como se sente hoje..." id="">
                 </textarea>
               </label>
 
               <label >
-                <input className={styles.inputs_hash} required value={tags} onChange={(e)=> setTags(e.target.value)} name="tags" maxLength='50' placeholder="Insira suas tags separadas por vírgula..." id="">
+                <input className={styles.inputs_hash} required value={tags} onChange={(e) => setTags(e.target.value)} name="tags" maxLength='50' placeholder="Insira suas tags separadas por vírgula..." id="">
                 </input>
               </label>
 
-              
+
 
               <label className={styles.btn_up_image}>
                 <img className={styles.putimg} src={putimgs} />
-                <input value={image} onChange={(e)=> setImage(e.target.value)}  className={styles.Inp_imageurl} type='text' placeholder="Digite a URL da imagem que combine com o seu post!" />
+                <input value={image} onChange={(e) => setImage(e.target.value)} className={styles.Inp_imageurl} type='text' placeholder="Digite a URL da imagem que combine com o seu post!" />
 
               </label>
 
-                {!response.loading &&
               <div className={styles.submit_box_post}>
-                <input   type='submit' value="Enviar Momento!" />
-              </div> }
-        
-          {response.loading && 
-          <div className={styles.submit_box_post}>
-            <input disabled type='submit' className={styles.disabled_submit} value="Enviando..." />
-          </div>}
 
-        <div className={styles.error}>
-          {response.error && <p> {response.error}</p>}
-        </div>
+                 <div className={styles.error}>
+                {response.error && <p> {response.error}</p>}
+                {formError && <p> {formError}</p>}
+              </div>
 
+              
+                {!response.loading &&
+                <label>
+                  <input type='submit' value="Enviar Momento!" />
+                </label>
+                }
 
-            </form>
+                {response.loading &&
+                <label>
+                  <input disabled type='submit' className={styles.disabled_submit} value="Enviando..." />
+                </label>
+                }
+
+              </div>
+            
+                        </form>
 
           </div>
         </div>
 
         <div className={styles.box_feed}>
+
+ 
+ {posts.map(post => (
+     <div className={styles.user_and_post} key={post.id}>
+            <div className={styles.profile_feed}>
+              <div className={styles.userprof_feed}>
+                <a><img className={styles.avatar_profile} src={avatar} /></a>
+              </div>
+            </div>
+
+            <div className={styles.content_post}>
+
+              {/* Fazer o map do nome e post do usuário */}
+
+              <div className={styles.userPost}>
+                <div className={styles.u_time}>
+                  <h3>{post.createdBy}</h3>
+                  
+                <span> {new Date(post.createdAt.seconds * 1000).toLocaleString("pt-BR")}</span>
+                </div>
+                <p >{post.body} </p>
+             
+             {post.image && 
+                 <img width="120px" src={post.image} />
+                 }<br />
+                 <span>#{post.tags}</span>
+              </div>
+            </div>
+
+          </div>
+
+   ))}
+
+
+
 
           <div className={styles.user_and_post}>
 
@@ -113,38 +200,14 @@ console.log(
 
               <div className={styles.userPost}>
                 <div className={styles.u_time}>
-                <h3>John</h3>
-                <span>20min</span>
-                </div>
-                <p >Hoje eu acordei feliz!</p>
-              </div>
-            </div>
-
-          </div>
-
-          
-          <div className={styles.user_and_post}>
-
-            <div className={styles.profile_feed}>
-              <div className={styles.userprof_feed}>
-                <a><img className={styles.avatar_profile} src={avatar} /></a>
-              </div>
-            </div>
-
-            <div className={styles.content_post}>
-
-              {/* Fazer o map do nome e post do usuário */}
-
-              <div className={styles.userPost}>
-                 <div className={styles.u_time}>
-                <h3>Clara</h3>
-                <span>20min</span>
+                  <h3>Clara</h3>
+                  <span>20min</span>
                 </div>
                 <p >Amanhã vou viajar.. 🌎</p>
               </div>
-              
+
             </div>
-            
+
           </div>
 
 
