aa3b6f73d3701c356a11b03383b8c26c210efe98
YanFelipy22 <yan-felipy@hotmail.com>
2025-06-25 01:44:38 -0300

    Revert "Post Form Component"
    This reverts commit 8dc28ea50885e51bfcf5516ce550dd621a87e602.

--- src/pages/Home Profile/HomeProfile.jsx
+++ src/pages/Home Profile/HomeProfile.jsx
@@ -12,59 +12,61 @@ import { useAuthValue } from '../../../context/AuthContext'
 
 const Home = () => {
   UseChangeTitle("Meus Momentos ")
-
+  
   const { user } = useAuthValue()
   const uid = user.uid
-  const { documents: posts } = useFetchDocuments("posts", null, uid)
-  const { deleteDocument } = useDeleteDocument("posts")
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid)
+  const {deleteDocument } = useDeleteDocument("posts")
 
-
+    
   return (
     <div className={styles.home_posts}>
       <aside className={styles.box_menu_profile}>
         <div className={styles.userprof_posts}>
 
-          <h4>Informações da conta </h4>
-
-          <img className={styles.avatar_profile} src={avatar} />
-          <div className={styles.user_info}>
-            <span>
-              {user.uid}
-            </span>
-            <h5>Nome :  {user.displayName} </h5>
+             <h4>Informações da conta </h4>
+                   
+                     <img className={styles.avatar_profile} src={avatar} />
+                  <div className={styles.user_info}>
+                    <span>
+                      {user.uid}
+                    </span>
+                    <h5>Nome :  {user.displayName} </h5>
             <h5>Email :  {user.email} </h5>
-          </div>
-        </div>
+                  </div>
+                 </div>
       </aside>
       <main className={styles.box_user_posts}>
         <div className={styles.post}>
-            {posts && posts.length === 0 ? (
-            <div className={styles.search_no_results}>
-              <h2>
-                Não encontramos nenhum post <span>:(</span>
-              </h2>
-              <p>
-                <Link className={styles.link_home} to="/"> Clique aqui para voltar página inicial e compartilhe os seus momentos conosco!
+
+{loading && <p className={styles.p_loading}>Carregando posts...</p>}
+
+ {posts && posts.length === 0 ?  (
+                <div className={styles.search_no_results}>
+                  <h2>
+                    Não encontramos nenhum post <span>:(</span>               </h2>
+                  <p>
+      <Link className={styles.link_home} to="/"> Clique aqui para voltar página inicial e compartilhe os seus momentos conosco!
                 </Link>
-              </p>
-
-            </div>
-          ) : (<div className={styles.search_results_ok}><p>Estes são os seus momentos compartilhados <span>{user.displayName}</span></p>
-          </div>)}
-
-
-
-
-
-
+                  </p>
+                  
+                </div>
+              ) : (<div className={styles.search_results_ok}><p>Estes são os seus momentos compartilhados<span>#{user.displayName}</span></p>
+              </div>) }
+          
+          
+          
+          
+          
+          
           {posts && posts.map((post) => (
 
-            <PostDetails event={() => deleteDocument(post.id)} key={post.id} post={post} />
+            <PostDetails event={()=>deleteDocument(post.id) }  key={post.id} post={post} desc={"Ler mais"} link={`/posts/${post.id}`} />
           ))}
-
+        
         </div>
 
-
+ 
       </main>
     </div>
   )
